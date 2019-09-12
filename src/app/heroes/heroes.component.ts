26a75d229adf24e7911a5f779a5f2927367bac5f
Sharma <anil.sharma@epicor.com>
2019-09-12 15:28:07 +0530

    routing commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from './../hero';
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,14 +10,26 @@ import { Hero } from './../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  // hero = 'Windstorm';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  constructor() { }
+
+  heroes: Hero[];
+  // selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
+
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  // }
 
 }
